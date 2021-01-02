7696ab4e888ca474d452bbb0649d4ebedf205635
init931 <friendskenny72@gmail.com>
2021-01-02 19:33:55 +0600

    alg quick sort

---
+++ scharp/CodeWarsSharp/CodeWarsSharp/Algorithms/QuickSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace CodeWarsSharp.Algorithms {
+    public class QuickSort {
+        public static void SortAsc(int[] A, int p, int r) {
+            if (p < r) {
+                var q = partition(A, p, r);
+                SortAsc(A, p, q - 1);
+                SortAsc(A, q + 1, r);
+            }
+        }
+
+        private static int partition(int[] A, int p, int r) {
+            int x = A[r];
+            int i = p - 1;
+            for (int j = p; j < r; j++) {
+                if (A[j] < x) {
+                    i = i + 1;
+                    var swap = A[i];
+                    A[i] = A[j];
+                    A[j] = swap;
+                }
+            }
+            var swap2 = A[i + 1];
+            A[i + 1] = A[r];
+            A[r] = swap2;
+            return i + 1;
+        }
+    }
+}
--- scharp/CodeWarsSharp/CodeWarsSharp/Program.cs
+++ scharp/CodeWarsSharp/CodeWarsSharp/Program.cs
@@ -8,6 +8,9 @@ namespace CodeWarsSharp {
     class Program {
         static void Main(string[] args) {
 
+            var t1 = new int[] { 2, 8, 7, 1, 3, 5, 6, 4 };
+            Algorithms.QuickSort.SortAsc(t1, 0, t1.Length - 1);
+
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
