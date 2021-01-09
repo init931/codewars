1deeb6ef511aea6904e6c2937a064a5242d3d02f
init931 <friendskenny72@gmail.com>
2021-01-09 15:31:54 +0600

    single responsibility principle

---
+++ scharp/CodeWarsSharp/CodeWarsSharp/DesignPatterns/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace CodeWarsSharp.DesignPatterns {
+    public class SingleResponsibilityPrinciple {
+        public SingleResponsibilityPrinciple() {
+            var shapes = new IShape[] {
+                new Circle(4),
+                new Square(10),
+                new Square(3)
+            };
+            var ac = new AreaCalculator(shapes);
+            Console.WriteLine($"area sum = {ac.Sum()}");
+        }
+
+        private interface IShape {
+
+        }
+
+        private class Square: IShape {
+            public int _length;
+
+            public Square(int length) {
+                _length = length;
+            }
+        }
+
+        private class Circle: IShape {
+            public int _radius;
+
+            public Circle(int radius) {
+                _radius = radius;
+            }
+        }
+
+        private class AreaCalculator {
+            private IShape[] _shapes;
+
+            public AreaCalculator(IShape[] shapes) {
+                _shapes = shapes;
+            }
+
+            public double Sum() {
+                double ret = 0;
+                foreach (var shape in _shapes) {
+                    if (shape is Square) {
+                        ret += Math.Pow(((Square)shape)._length, 2);
+                    }
+                    else if (shape is Circle) {
+                        ret += Math.PI * Math.Pow(((Circle)shape)._radius, 2);
+                    }
+                }
+                return ret;
+            }
+        }
+    }
+}
--- scharp/CodeWarsSharp/CodeWarsSharp/Program.cs
+++ scharp/CodeWarsSharp/CodeWarsSharp/Program.cs
@@ -8,8 +8,7 @@ namespace CodeWarsSharp {
     class Program {
         static void Main(string[] args) {
 
-            var t1 = new int[] { 2, 5, 3, 0, 2, 3, 0, 3 };
-            t1 = Algorithms.CountingSort.SortAsc(t1, new int[t1.Length + 1], t1.Max());
+            new DesignPatterns.SingleResponsibilityPrinciple();
 
             Console.WriteLine("Hello World!");
             Console.ReadLine();
