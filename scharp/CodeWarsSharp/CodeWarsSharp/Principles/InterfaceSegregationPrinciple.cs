9b1a72e3ff52566797689fe3433a912f8d843b3e
init931 <friendskenny72@gmail.com>
2021-01-21 19:34:35 +0600

    interface segregation principle

---
+++ scharp/CodeWarsSharp/CodeWarsSharp/Principles/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace CodeWarsSharp.Principles {
+    public class InterfaceSegregationPrinciple {
+        public class ViolatePrinciple {
+            interface ShapeInterface {
+                public int area();
+
+                public int volume(); // add new method
+            }
+
+            class Square : ShapeInterface {
+                public int area() {
+                    throw new NotImplementedException();
+                }
+
+                public int volume() {
+                    // violation of ISP
+                    return -1;
+                }
+            }
+
+            class Cuboid : ShapeInterface {
+                public int area() {
+                    throw new NotImplementedException();
+                }
+
+                public int volume() {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        public class FollowPrinciple {
+            interface ShapeInterface {
+                public int area();
+            }
+
+            interface ThreeDimensionalShapeInterface { // different interface
+                public int volume();
+            }
+
+            class Cuboid : ShapeInterface, ThreeDimensionalShapeInterface {
+                public int area() {
+                    return 10;
+                }
+
+                public int volume() {
+                    return 10;
+                }
+            }
+        }
+    }
+}
