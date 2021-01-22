dcc9eec978603baf8a29edda10c20ab42c6f05cd
init931 <friendskenny72@gmail.com>
2021-01-22 15:26:22 +0600

    dependency inversion principle

---
+++ scharp/CodeWarsSharp/CodeWarsSharp/Principles/DependencyInversionPrinciple.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace CodeWarsSharp.Principles {
+    public class DependencyInversionPrinciple {
+        public class ViolatePrinciple {
+            class MySQLConnection {
+                public void connect() {
+                    // handle the database connection
+                }
+            }
+
+            class PasswordReminder {
+                private MySQLConnection _dbConnection;
+
+                public PasswordReminder(MySQLConnection dbConnection) { //violation
+                    _dbConnection = dbConnection;
+                }
+            }
+        }
+
+        public class FollowPrinciple {
+            interface DBConnectionInterface {
+                public void connect();
+            }
+
+            class MySQLConnection : DBConnectionInterface {
+                public void connect() {
+                    // handle the database connection
+                }
+            }
+
+            class PasswordReminder {
+                private DBConnectionInterface _dbConnection;
+
+                public PasswordReminder(DBConnectionInterface dbConnection) { //fixed
+                    _dbConnection = dbConnection;
+                }
+            }
+        }
+    }
+}
