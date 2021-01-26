57903e3c34726a8d787ee78a38e44476ff685a98
init931 <friendskenny72@gmail.com>
2021-01-26 21:37:19 +0600

    factory method pattern

---
+++ scharp/CodeWarsSharp/CodeWarsSharp/Patterns/FactoryMethod.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsSharp.Patterns.FactoryMethod {
+    class FactoryMethod {
+        public FactoryMethod() {
+            var fanFactory = new Fan2Factory();
+            var fan2 = fanFactory.CreateFan();
+            fan2.SwitchOn();
+        }
+    }
+
+    interface IFan {
+        void SwitchOn();
+        void SwitchOff();
+    }
+
+    class Fan1 : IFan {
+        public void SwitchOff() {
+        }
+
+        public void SwitchOn() {
+        }
+    }
+
+    class Fan2 : IFan {
+        public void SwitchOff() {
+        }
+
+        public void SwitchOn() {
+        }
+    }
+
+    interface IFanFactory {
+        IFan CreateFan();
+    }
+
+    class Fan1Factory : IFanFactory {
+        public IFan CreateFan() {
+            return new Fan1();
+        }
+    }
+
+    class Fan2Factory : IFanFactory {
+        public IFan CreateFan() {
+            return new Fan2();
+        }
+    }
+}
